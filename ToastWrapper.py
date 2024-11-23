bf7a382cfce179ff976de04f07458c0d53fd4a17
CodyNinja1 <ahmadisntcool@gmail.com>
2024-11-23 10:33:36 +0300

    added color palettes

--- ToastWrapper.py
+++ ToastWrapper.py
@@ -1,3 +1,4 @@
 from win11toast import toast
+from SettingsHandler import GetSettings
 def Notify(Title: str, Description: str) -> bool:
-    return type(toast(Title, Description, on_click="http:", app_id="ChannelNotif")) == dict
+    return type(toast(Title, Description, on_click="http:", app_id=GetSettings().Notifications.AppId)) == dict
